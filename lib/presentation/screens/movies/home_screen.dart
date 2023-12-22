295d31e08b7ae611962831cb6c6a3fac72e5bbfa
fritz <franciscosantin2002@gmail.com>
2023-12-22 00:33:28 -0300

    Pantalla de carga

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -34,6 +34,9 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if (initialLoading) return const FullScreenLoader();
+
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
     final popularMovies = ref.watch(popularMoviesProvider);
@@ -42,6 +45,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     if (slideShowMovies.isEmpty) {
       return const Center(child: CircularProgressIndicator());
     }
+
     return CustomScrollView(slivers: [
       const SliverAppBar(
         floating: true,
--- lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -59,7 +59,8 @@ class _MoviesHorizontalListviewState extends State<MoviesHorizontalListview> {
                     scrollDirection: Axis.horizontal,
                     physics: const BouncingScrollPhysics(),
                     itemBuilder: (context, index) {
-                      return _Slide(movie: widget.movies[index]);
+                      return FadeInRight(
+                          child: _Slide(movie: widget.movies[index]));
                     }))
           ],
         ));
