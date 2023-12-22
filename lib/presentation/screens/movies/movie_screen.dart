8751674a888f673de62f6f4cad80ca7e6c35b005
fritz <franciscosantin2002@gmail.com>
2023-12-22 15:00:33 -0300

    movie_screen terminada

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,7 +1,9 @@
-import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:animate_do/animate_do.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
   static const String name = 'movie_screen';
@@ -16,6 +18,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
   @override
   void initState() {
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
     super.initState();
   }
 
@@ -85,7 +88,8 @@ class _MovieDetails extends StatelessWidget {
                             borderRadius: BorderRadius.circular(20)))))
               ],
             )),
-        const SizedBox(height: 100)
+        _ActorsByMovie(movieId: movie.id),
+        const SizedBox(height: 50)
       ],
     );
   }
@@ -104,15 +108,22 @@ class _CustomSliverAppBar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
           titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          title: Text(
-            movie.title,
-            style: const TextStyle(fontSize: 20),
-            textAlign: TextAlign.start,
-          ),
+          // title: Text(
+          //   movie.title,
+          //   style: const TextStyle(fontSize: 20),
+          //   textAlign: TextAlign.start,
+          // ),
           background: Stack(
             children: [
               SizedBox.expand(
-                  child: Image.network(movie.posterPath, fit: BoxFit.cover)),
+                  child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) return const SizedBox();
+                  return FadeIn(child: child);
+                },
+              )),
               const SizedBox.expand(
                 child: DecoratedBox(
                     decoration: BoxDecoration(
@@ -133,5 +144,51 @@ class _CustomSliverAppBar extends StatelessWidget {
             ],
           )),
     );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+  final int movieId;
+
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+    if (actorsByMovie[movieId.toString()] == null) {
+      return const CircularProgressIndicator(strokeWidth: 2);
+    }
+    final actors = actorsByMovie[movieId.toString()]!;
+    return SizedBox(
+        height: 300,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: actors.length,
+            itemBuilder: (context, index) {
+              final actor = actors[index];
+              return Container(
+                  padding: const EdgeInsets.all(8.0),
+                  width: 135,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //Actor Photo
+                      FadeInRight(
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.network(actor.profilePath,
+                                height: 180, width: 135, fit: BoxFit.cover)),
+                      ),
+                      //Actor nombre
+                      const SizedBox(height: 5),
+                      Text(actor.name, maxLines: 2),
+                      Text(actor.character ?? '',
+                          maxLines: 2,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              overflow: TextOverflow.ellipsis))
+                    ],
+                  ));
+            }));
   }
 }
